af471a1b236b8b74fbbd3b5b1f74dcc76977a963
Panji Bayu <74200519+bypanji@users.noreply.github.com>
2025-08-17 18:03:01 +0700

    merge k list

--- solution14.js
+++ solution14.js
@@ -1,14 +1,10 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode[]} lists
- * @return {ListNode}
- */
+class ListNode {
+    constructor(val, next = null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
 const lists = [[1,4,5],[1,3,4],[2,6]]
 
 var mergeKLists = function(lists) {
