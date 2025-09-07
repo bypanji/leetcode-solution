4c00927e1864e2b0ea5358931bb1e295c8b0b398
bypanji <panjiprasena@gmail.com>
2025-09-07 22:58:22 +0700

    sum zero

---
+++ solution28.js
@@ -0,0 +1,12 @@
+const n = 5;
+
+var sumZero = function(n) {
+    const result = [];
+    for (let i = 1; i <= Math.floor(n / 2); i++) {
+        result.push(i, -i);
+    }
+    if (n % 2 !== 0) {
+        result.push(0);
+    }
+    return result;
+};
