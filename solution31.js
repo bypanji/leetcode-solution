a840cf166351ec25e3ce8bed0e5957933defb33f
bypanji <panjiprasena@gmail.com>
2025-09-11 09:08:44 +0700

    Is Anagram

--- solution31.js
+++ solution31.js
@@ -3,7 +3,6 @@ const t = "nagaram"
 
 var isAnagram = function(s, t) {
     if(s.length !== t.length) return false
-    if(s.split("").sort().join("") === t.split("").sort().join("")) return true
 
-    return false
+    return s.split("").sort().join("") === t.split("").sort().join("")
 };
